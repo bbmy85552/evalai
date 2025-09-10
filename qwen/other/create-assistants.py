f8023fb2a95acd38f03bd151086b1b77233f7cf3
biemor <98942169+biemor@users.noreply.github.com>
2025-09-11 01:15:58 +0800

    Update create-assistants.py

--- qwen/other/create-assistants.py
+++ qwen/other/create-assistants.py
@@ -1,14 +1,12 @@
 from dashscope import Assistants
 import os
-import dashscope
 from dotenv import load_dotenv
 
 load_dotenv()
-dashscope.base_http_api_url = 'https://dashscope-intl.aliyuncs.com/api/v1'
 assistant = Assistants.create(
         # 建议您优先配置环境变量。若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
         api_key=os.getenv("DASHSCOPE_API_KEY"),
-        # 此处以qwen-max为例，可按需更换模型名称。模型列表：https://www.alibabacloud.com/help/zh/model-studio/getting-started/models
+        # 此处以qwen-max为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
         model='qwen-max',
         name='smart helper',
         description='A tool helper.',
